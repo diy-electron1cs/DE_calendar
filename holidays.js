685570843651e78084439ad8d7a91c5b8dac4a05
DIY электроника <private.property240702@gmail.com>
2025-09-13 14:58:31 +0300

    Update holidays.js

--- holidays.js
+++ holidays.js
@@ -105,8 +105,8 @@ else if (day === 4 && month === 11) {
     //день народного единства, 04.11
     addImage('center-top', 'https://github.com/diy-electron1cs/DE_calendar/blob/main/images/4-now.png?raw=true', '150px', '0px', '0px');
 }
-//else if (true) {
+
 else if (isProgrammersDay(day, month) === true) {
     //день программиста, 256 посчёту
-    setResponsiveBackground('https://github.com/diy-electron1cs/DE_calendar/blob/main/images/matrix.png?raw=true');
+    setResponsiveBackground('https://github.com/diy-electron1cs/DE_calendar/blob/main/images/MATRIX.gif?raw=true');
 }
